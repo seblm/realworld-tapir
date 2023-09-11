0e9678e9a38ce30420516738fe47d3f2d19a485e
Sébastian Le Merdy <567637+seblm@users.noreply.github.com>
2023-09-11 21:07:45 +0200

    :sparkles: follow and unfollow a user

--- src/main/scala/realword/tapir/profile/ProfileEndpoints.scala
+++ src/main/scala/realword/tapir/profile/ProfileEndpoints.scala
@@ -2,6 +2,7 @@ package realword.tapir.profile
 
 import io.circe.generic.auto.*
 import sttp.model.Method
+import sttp.model.Method.{DELETE, POST}
 import sttp.tapir.*
 import sttp.tapir.docs.apispec.DocsExtensionAttribute.*
 import sttp.tapir.generic.auto.*
@@ -17,9 +18,25 @@ object ProfileEndpoints:
     .summary("Get a profile")
     .description("Get a profile of a user of the system. Auth is optional")
     .out(profileResponse)
+  private val followUserByUsername = endpoint
+    .in("profiles" / path[String]("username").description("Username of the profile you want to follow") / "follow")
+    .post
+    .tag(tag)
+    .summary("Follow a user")
+    .description("Follow a user by username")
+    .out(profileResponse)
+  private val unfollowUserByUsername = endpoint
+    .in("profiles" / path[String]("username").description("Username of the profile you want to unfollow") / "follow")
+    .delete
+    .tag(tag)
+    .summary("Unfollow a user")
+    .description("Unfollow a user by username")
+    .out(profileResponse)
 
-  val endpoints: Iterable[AnyEndpoint] = List(getProfileByUsername)
+  val endpoints: Iterable[AnyEndpoint] = List(getProfileByUsername, followUserByUsername, unfollowUserByUsername)
 
   def operationIdGenerator: PartialFunction[(AnyEndpoint, Vector[String], Method), String] = {
-    case (_, Vector("profiles", "username"), _) => "GetProfileByUsername"
+    case (_, Vector("profiles", "username"), _)                => "GetProfileByUsername"
+    case (_, Vector("profiles", "username", "follow"), POST)   => "FollowUserByUsername"
+    case (_, Vector("profiles", "username", "follow"), DELETE) => "UnfollowUserByUsername"
   }
